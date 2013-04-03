9a8c4cf8e7201976e49461d52c1abf65bb0b90c6
Ha Son Hai <hasonhai124@gmail.com>
2013-04-03 16:55:14 +0200

    Update multi-test run script

--- RunMultiTest.sh
+++ RunMultiTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # For running a collection of test at night
-# Usage: ./NightRun.sh > /dev/null &> NightRun.console &
+# Usage: ./RunMultiTest.sh > /dev/null &> RunMultiTest.console &
 
 CHANGE_TCP_HTCP="sysctl -w net.ipv4.tcp_congestion_control=htcp"
 CHANGE_TCP_VEGAS="sysctl -w net.ipv4.tcp_congestion_control=vegas"
@@ -10,29 +10,64 @@ CHANGE_TCP_CUBIC="sysctl -w net.ipv4.tcp_congestion_control=cubic"
 CHANGE_TCP_RENO="sysctl -w net.ipv4.tcp_congestion_control=reno"
 
 CMD=ssh
-CMD_OPTIONS="root@134.59.129.197"
+SVR_OPTIONS="root@134.59.129.197"
+CLT_OPTIONS="root@10.10.11.1"
 
 #Test RENO
-$CHANGE_TCP_RENO
-$CMD $CMD_OPTIONS "$CHANGE_TCP_RENO"
-for INCR in `seq 1 10`
+$CMD $CLT_OPTIONS "$CHANGE_TCP_RENO"
+$CMD $SVR_OPTIONS "$CHANGE_TCP_RENO"
+for INCR in `seq 1 3`
 do
    sleep 10
-   sh RunTest.sh 32 tshark
+   sh RunTestRemote.sh 32 tshark
+done
+
+#Test CUBIC
+$CMD $CLT_OPTIONS "$CHANGE_TCP_CUBIC"
+$CMD $SVR_OPTIONS "$CHANGE_TCP_CUBIC"
+for INCR in `seq 1 3`
+do
+   sleep 10
+   sh RunTestRemote.sh 32 tshark
+done
+
+#Test Highspeed
+$CMD $CLT_OPTIONS "$CHANGE_TCP_HIGHSPEED"
+$CMD $SVR_OPTIONS "$CHANGE_TCP_HIGHSPEED"
+for INCR in `seq 1 3`
+do
+   sleep 10
+   sh RunTestRemote.sh 32 tshark
 done
 
 ADDING_DELAY="tc qdisc add dev em2 root netem delay 200ms && echo Delay is added"
 DEL_DELAY="tc qdisc del dev em2 root netem delay 200ms && echo Delay is removed"
 
-$CMD $CMD_OPTIONS "$ADDING_DELAY"
+$CMD $SVR_OPTIONS "$ADDING_DELAY"
 
-$CHANGE_TCP_RENO
-$CMD $CMD_OPTIONS "$CHANGE_TCP_RENO"
-for INCR in `seq 1 10`
+$CMD $CLT_OPTIONS "$CHANGE_TCP_RENO"
+$CMD $SVR_OPTIONS "$CHANGE_TCP_RENO"
+for INCR in `seq 1 1`
 do
-    sh RunTest.sh 32 tshark
+    sh RunTestRemote.sh 32 tshark
     sleep 10
 done
 
-$CMD $CMD_OPTIONS "$DEL_DELAY"
+$CMD $CLT_OPTIONS "$CHANGE_TCP_CUBIC"
+$CMD $SVR_OPTIONS "$CHANGE_TCP_CUBIC"
+for INCR in `seq 1 1`
+do
+    sh RunTestRemote.sh 32 tshark
+    sleep 10
+done
+
+$CMD $CLT_OPTIONS "$CHANGE_TCP_HIGHSPEED"
+$CMD $SVR_OPTIONS "$CHANGE_TCP_HIGHSPEED"
+for INCR in `seq 1 1`
+do
+    sh RunTestRemote.sh 32 tshark
+    sleep 10
+done
+
+$CMD $SVR_OPTIONS "$DEL_DELAY"
 echo Done\!
